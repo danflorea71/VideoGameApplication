1f7960f655731647d26e6b133eb06eeaced1489d
Dan FLOREA <Dan.FLOREA@bdc.ca>
2020-05-26 19:51:26 -0400

    Create the first version of the project

---
+++ VideoGameApplication/Migrations/20200526232621_FirstVersionDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoGameApplication.Migrations
+{
+    public partial class FirstVersionDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoGame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    PublicationYear = table.Column<int>(nullable: false),
+                    MinimumPlayers = table.Column<int>(nullable: false),
+                    MaximumPlayers = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGame", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoGame");
+        }
+    }
+}
